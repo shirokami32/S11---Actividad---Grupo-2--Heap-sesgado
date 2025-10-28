5443883cf6b8375b6000d6f8e2a45be2a119ccba
ManuAFK28 <manustdev@gmail.com>
2025-10-27 23:45:07 -0500

    Refactor heap_Sesgado to use Incidente pointers
    Updated Nodo structure to use Incidente pointer. Modified eliminarMin and verMin functions to handle Incidente pointers and added merge function.

--- heap_Sesgado.h
+++ heap_Sesgado.h
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 
 struct Incidente {
     int prioridad;
@@ -11,16 +13,48 @@ struct Incidente {
 };
 
 struct Nodo {
-    Incidente dato;
+    Incidente* incidente;
     Nodo* izq;
     Nodo* der;
 
-    Nodo(Incidente i) : dato(i), izq(nullptr), der(nullptr) {}
+    Nodo(Incidente* i)
+        : incidente(i), izq(nullptr), der(nullptr) {}
 };
+
+Nodo* merge(Nodo* h1, Nodo* h2);
 
 Nodo* insertar(Nodo* heap, Incidente incidente) {
     Nodo* nuevo = new Nodo(incidente);
     return merge(heap, nuevo);
+}
+
+Nodo* eliminarMin(Nodo* heap){
+    if (!heap) {
+        cout << "\nNo hay incidentes que atender.\n";
+        return nullptr;
+    }
+    
+    cout << "\nIncidente atendido:\n";
+    cout << "   Descripcion: " << heap->incidente->descripcion << endl;
+    cout << "   Ubicacion: " << heap->incidente->ubicacion << endl;
+    cout << "   Prioridad: " << heap->incidente->prioridad << endl;
+    
+    Nodo *nuevoNodo = merge(head->izq, head->der);
+    delete heap->incidente;
+    delete heap;
+    return nuevoHeap;
+}
+
+// Visualizar el incidente más urgente sin eliminarlo
+void verMin(Nodo* heap) {
+    if (!heap) {
+        cout << "\nNo hay incidentes pendientes.\n";
+        return;
+    }
+    cout << "\nIncidente mas urgente:\n";
+    cout << "   Descripcion: " << heap->incidente->descripcion << endl;
+    cout << "   Ubicacion: " << heap->incidente->ubicacion << endl;
+    cout << "   Prioridad: " << heap->incidente->prioridad << endl;
 }
 
 Nodo* merge(Nodo* h1, Nodo* h2) {
@@ -35,12 +69,7 @@ Nodo* merge(Nodo* h1, Nodo* h2) {
     return h1;
 }
 
-Nodo* eliminarMin(Nodo* head){
-    if(!head) return nullptr;
-    Nodo *nuevoNodo = merge(head->izq, head->der);
-    delete head;
-    return nuevoNodo;
-}
+
 
 void printHeap(Nodo* heap, int padre){
     if(!heap) return;
