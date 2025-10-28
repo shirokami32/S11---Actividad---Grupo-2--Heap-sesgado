4344cbc1805f428d024fb3e2c8e79adc2093bd6f
Adrian Ortiz Rodriguez <134827772+shirokami32@users.noreply.github.com>
2025-10-28 00:02:04 -0500

    Implementacion del merge y creacion del menu

--- heap_Sesgado.h
+++ heap_Sesgado.h
@@ -57,4 +57,44 @@ void verMin(Nodo* heap) {
     cout << "   Prioridad: " << heap->incidente->prioridad << endl;
 }
 
+Nodo* merge(Nodo* h1, Nodo* h2) {
+    if (!h1) return h2;
+    if (!h2) return h1;
+
+    if (h1->incidente->prioridad > h2->incidente->prioridad)
+        swap(h1, h2);
+
+    h1->der = merge(h1->der, h2);
+    swap(h1->izq, h1->der);
+
+    return h1;
+}
+
+void printHeap(Nodo* heap, int nivel = 0, string lado = "Raiz") {
+    if (!heap) return;
+
+    for (int i = 0; i < nivel; i++)
+        cout << "   ";
+
+    cout << "-> [" << lado << "] "
+         << "(Prioridad: " << heap->incidente->prioridad << ") "
+         << heap->incidente->descripcion
+         << " | " << heap->incidente->ubicacion << endl;
+
+    if (heap->izq)
+        printHeap(heap->izq, nivel + 1, "Izq");
+    if (heap->der)
+        printHeap(heap->der, nivel + 1, "Der");
+}
+
+void mostrarMenu() {
+    cout << "\n===== SISTEMA DE EMERGENCIAS=====\n";
+    cout << "1. Insertar nuevo incidente\n";
+    cout << "2. Atender incidente mas urgente\n";
+    cout << "3. Ver incidente mas urgente\n";
+    cout << "4. Fusionar zonas\n";
+    cout << "5. Mostrar estructura del heap\n";
+    cout << "6. Salir\n";
+    cout << "Seleccione una opcion: ";
+}
 
